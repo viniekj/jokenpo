96d23890b7afcf7ec68608cb0b2f62627cfef7eb
Vinicius <vinicius.schuck2@hotmail.com>
2022-10-06 21:15:56 -0300

    Versão 1
    Primeira versão de jokenpô :D

--- app.js
+++ app.js
@@ -1,11 +1,67 @@
-const choiceButtons = document.querySelectorAll('[data-choices]');
+const choices = document.querySelectorAll(".choice");
+const resultDisplay = document.getElementById("result")
+let userChoice;
+let computerChoice;
+let result;
 
-choiceButtons.forEach(choiceBtn => {
-    choiceBtn.addEventListener('click',() => {
-        const choiceName = choiceBtn.dataset.choices;
-        makeaChoice(choiceName);
+
+choices.forEach(button => {
+    button.addEventListener("click",(e) => {
+    userChoice = e.target.id
+    makeComputerChoice()
+    winCondition()
     })
 })
-function makeaChoice(choice){
-    console.log(choice)
+
+function makeComputerChoice(){
+    const randomNum = Math.floor(Math.random() * choices.length)
+    
+    if(randomNum === 0){
+        computerChoice = "rock"
+    }
+    if(randomNum === 1){
+        computerChoice = "paper"
+    }
+    if(randomNum === 2){
+        computerChoice = "scissors"
+    }
+    
 }
+
+function winCondition(){
+    if(computerChoice === userChoice){
+        result = "Empate"
+    }
+    if(computerChoice === "rock" && userChoice ==="paper"){
+        result = "Você Venceu!"
+    }
+    if(computerChoice === "paper" && userChoice ==="scissors"){
+        result = "Você Venceu!"
+    }
+    if(computerChoice === "scissors" && userChoice ==="rock"){
+        result = "Você Venceu!"
+    }
+    if(computerChoice === "paper" && userChoice ==="rock"){
+        result = "Você Perdeu :("
+    }
+    if(computerChoice === "rock" && userChoice ==="scissors"){
+        result = "Você Perdeu :("
+    }
+    if(computerChoice === "scissors" && userChoice === "paper"){
+        result = "Você Perdeu :("
+    }
+    resultDisplay.innerHTML = result
+
+    if(result === "Você Venceu!"){
+        resultDisplay.style.color = "green"
+    }else if (result ==="Você Perdeu :("){
+        resultDisplay.style.color = "red"
+    }else{
+        resultDisplay.style.color = "orange"
+    }
+    
+}
+ 
+
+
+
