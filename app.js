83919e42e6122a8a2581974b17d1ae73a4fd3ff2
Vinicius <vinicius.schuck2@hotmail.com>
2022-11-01 19:02:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const choices = document.querySelectorAll(".choice");
-const resultDisplay = document.getElementById("result")
+const resultDisplay = document.getElementById("result");
+const computerDisplay = document.getElementById("computerDisplay");
+const playerDisplay = document.getElementById("playerDisplay");
 let userChoice;
 let computerChoice;
 let result;
 
 
+
+
 choices.forEach(button => {
     button.addEventListener("click",(e) => {
     userChoice = e.target.id
+    playerDisplay.innerHTML = userChoice
     makeComputerChoice()
     winCondition()
     })
@@ -17,14 +22,16 @@ function makeComputerChoice(){
     const randomNum = Math.floor(Math.random() * choices.length)
     
     if(randomNum === 0){
-        computerChoice = "rock"
+        computerChoice = "Pedra"
     }
     if(randomNum === 1){
-        computerChoice = "paper"
+        computerChoice = "Papel"
     }
     if(randomNum === 2){
-        computerChoice = "scissors"
+        computerChoice = "Tesoura"
     }
+    computerDisplay.innerHTML = computerChoice
+    
     
 }
 
@@ -32,22 +39,22 @@ function winCondition(){
     if(computerChoice === userChoice){
         result = "Empate"
     }
-    if(computerChoice === "rock" && userChoice ==="paper"){
+    if(computerChoice === "Pedra" && userChoice ==="Papel"){
         result = "Você Venceu!"
     }
-    if(computerChoice === "paper" && userChoice ==="scissors"){
+    if(computerChoice === "Papel" && userChoice ==="Tesoura"){
         result = "Você Venceu!"
     }
-    if(computerChoice === "scissors" && userChoice ==="rock"){
+    if(computerChoice === "Tesoura" && userChoice ==="Pedra"){
         result = "Você Venceu!"
     }
-    if(computerChoice === "paper" && userChoice ==="rock"){
+    if(computerChoice === "Papel" && userChoice ==="Pedra"){
         result = "Você Perdeu :("
     }
-    if(computerChoice === "rock" && userChoice ==="scissors"){
+    if(computerChoice === "Pedra" && userChoice ==="Tesoura"){
         result = "Você Perdeu :("
     }
-    if(computerChoice === "scissors" && userChoice === "paper"){
+    if(computerChoice === "Tesoura" && userChoice === "Papel"){
         result = "Você Perdeu :("
     }
     resultDisplay.innerHTML = result
